287e358f364e3332fe27f6aa5461af10ff0ffb74
Christoph <christoph.sax@gmail.com>
2013-09-19 13:08:58 +0200

    two way parsing from X13 syntax to R

--- R/parse.R
+++ R/parse.R
@@ -1,4 +1,5 @@
-ParseCurly <- function(txt){
+#' @export
+ReadText <- function(txt){
   require(stringr)
   mat <- str_match_all(txt, '(?:([a-z]+))\\{(.*?)\\}')[[1]]
   z <- mat[,3]
@@ -7,11 +8,82 @@ ParseCurly <- function(txt){
   str_trim(z)
 }
 
-ParseRound <- function(txt){
+#' @export
+ReadSpec <- function(txt){
   require(stringr)
-  mat <- str_match_all(txt, '(?:([a-z]+))\\s?=\\s?\\((.*?)\\s?\\)')[[1]]
-  z <- str_trim(str_replace_all(mat[,3], pattern = '\\s+', " "))
-  z <- str_split(z, pattern = " ")
-  names(z) <- mat[,2]
+  st <- str_split(txt, '=')[[1]]  # split at '='
+  
+  if (length(st) > 2){
+    st.red <- st[2:(length(st)-1)]  # exclude last and first element
+    sl.red <- str_match_all(st.red, '(.*)\\s([a-z]+)')  # match to a list
+    sm.red <- do.call(rbind, sl.red)  # convert to a matrix
+    
+    z <- c(sm.red[,2], st[length(st)])  # combine with the excluded last content
+    names(z) <- c(st[1], sm.red[,3])  # combine with the excluded first name
+  } else if (length(st) == 2){
+    z <- st[2]
+    names(z) <- st[1]
+  } else {
+    z <- NULL
+  }
   z
+}
+
+#' @export
+ReadElement <- function(x){
+  # remove curved brackets
+  x.nb <- str_replace_all(x, '[\\(\\)]', ' ')
+  
+  # slit along spaces (if not double quoted)
+  if (!str_detect(x.nb, '[\\"].*[\\"]')){
+    z <- str_split(str_trim(x.nb), '\\s+')[[1]]
+  } else {
+    z <- x.nb
+  }
+  
+  # convert to numeric if possible
+  try.numeric <- suppressWarnings(as.numeric(z))
+  if (!any(is.na(try.numeric))){
+    z <- as.numeric(z)
+  }
+  z
+}
+
+#' @export
+ReadX13 <- function(file){
+  txt = paste(readLines(file), collapse = " ")
+  curly.txt <- ReadText(txt)
+  z <- lapply(curly.txt, ReadSpec)
+  lapply(z, function(el) lapply(el, ReadElement))
+}
+
+#' @export
+WriteSpec <- function(x){
+  nx <- names(x)
+  z <- character(length = length(x))
+  for (i in seq_along(x)){
+    if (length(x[[i]]) > 1){
+      # only put brackets around several elements
+      z[i] <- paste0("  ", nx[i], " = (", paste(x[[i]], collapse = " "), ")")
+    } else if (length(x[[i]] == 1)){
+      z[i] <- paste0("  ", nx[i], " = ", x[[i]])
+    } else {
+      z[i] <- ""
+    }
+  }
+  paste(z, collapse = "\n")
+}
+
+
+#' @export
+WriteText <- function(x){
+  xl <- lapply(x, WriteSpec)
+  paste(paste0(names(x), "{\n", xl, "\n}"), collapse = "\n\n")
+}
+
+#' @export
+WriteX13 <- function(x, file){
+  stopifnot(inherits(x, "list"))
+  txt <- WriteText(x)
+  writeLines(txt, con = file)
 }
--- inst/exp/start.R
+++ inst/exp/start.R
@@ -1,18 +1,8 @@
 devtools::load_all(".")
 syspath <- paste0(system.file(package = "seasonal"), "/inst/io/")  # if load.all
-
 infile <- paste0(syspath, "test.mdl")
+outfile <- paste0(syspath, "test2.mdl")
 
-txt = paste(readLines(infile), collapse=" ")
-p <- ParseCurly(txt)
-
-
-rr <- ParseRound(p['regression'])
-as.numeric(rr$b)
-
-aa <- ParseRound(p['arima'])
-as.numeric(aa$ma)
-
-
-
+y <- ReadX13(infile)
+WriteX13(y, file = outfile)
 
