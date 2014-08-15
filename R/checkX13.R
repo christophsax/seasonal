1c75bcdb26436ded0700dd8bae89ae6ceac18d0c
Christoph <christoph.sax@gmail.com>
2014-08-15 11:58:15 +0200

    dont link to vignette as it is temp. removed for beta testing

--- R/checkX13.R
+++ R/checkX13.R
@@ -25,7 +25,7 @@
 checkX13 <- function(fail = FALSE, fullcheck = TRUE, htmlcheck = TRUE){
   ### check path
   no.path.message <- "No path to the binary executable of X-13 specified.
-  \nFor installation details, consider Section 2 of the package vignette:\n  vignette(\"seas\")\n"
+  \nFor installation details, consider Section 2 of the package vignette:\n  https://github.com/christophsax/seasonal\n"
   env.path <- Sys.getenv("X13_PATH")
   if (env.path == ""){
     if (fail){
