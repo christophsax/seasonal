44ec5aa302abbb1853b8b4fa7074edd03c6a4c26
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-01-02 21:44:41 +0100

    dealing with solaris

--- R/startup.R
+++ R/startup.R
@@ -5,6 +5,15 @@
     if (.Platform$OS.type == "windows"){    
       x13.bin.html <- file.path(default.path, "x13ashtml.exe")
     } else {
+      # Solaris users get this message
+      if (!Sys.info()["sysname"] %in% c("Darwin", "Linux")){
+      return(packageStartupMessage("Unusual platform: ", Sys.info()["sysname"], 
+        "\nFor this platform, the path to the binary executable of X-13",
+        "\nhas to be manually specified. For more information, consider", 
+        "\nAppendix A of the package vignette:",
+        "\n  vingnette(seas)\n")
+      )
+      }
       x13.bin.html <- file.path(default.path, "x13ashtml")
     }
 
@@ -13,13 +22,17 @@
       env.path = default.path
     }
   } else {
-    packageStartupMessage(
-      "The system variable 'X13_PATH' has been manually set to: ", 
-      Sys.getenv("X13_PATH"),
-      "\n\nSince version 1.2, 'seasonal' uses the binaries from the 'x13binary' 
-package, and does not require 'X13_PATH' to be set. Only set 'X13_PATH' 
-manually if you want to use your own compilation."
-    )
+    # Solaris users don't get this message
+    if ((.Platform$OS.type == "windows") || (Sys.info()["sysname"] %in% c("Darwin", "Linux"))){
+      packageStartupMessage(
+        "The system variable 'X13_PATH' has been manually set to: \n  ", 
+        Sys.getenv("X13_PATH"),
+        "\n\nFrom version 1.2 on, 'seasonal' uses the X-13 binaries", 
+        "\nprovided by the 'x13binary' package, and does not require ",
+        "\n'X13_PATH' to be set anymore. Only set 'X13_PATH' manually ",
+        "\nif you intend to use your own binaries."
+      )
+    }
   }
 
   checkX13(fullcheck = FALSE, htmlcheck = TRUE)
