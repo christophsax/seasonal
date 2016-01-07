443c4357a86d4b14c35e1b0b8fd14ee5f1c516ce
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-01-07 14:15:35 +0100

    rely on x13binary::supportedPlatform()

--- R/startup.R
+++ R/startup.R
@@ -1,27 +1,25 @@
 .onLoad <- function(...){
   if (Sys.getenv("X13_PATH") != ""){
-    if ((.Platform$OS.type == "windows") || (Sys.info()["sysname"] %in% c("Darwin", "Linux"))){
-      # Also skip this message if X13_PATH is set to x13binary.path
-
-
-      # til we have x13binary::x13path()
-      if (Sys.getenv("X13_PATH") != system.file("bin", package="x13binary")){
+    if (x13binary::supportedPlatform()){
+      # skip this message if X13_PATH is set to x13binary.path
+      if (Sys.getenv("X13_PATH") != x13binary::x13path()){
       # if (Sys.getenv("X13_PATH") != x13binary::x13path()){
         packageStartupMessage(
           "The system variable 'X13_PATH' has been manually set to: \n  ", 
           Sys.getenv("X13_PATH"),
-          "\n\nFrom version 1.2 on, 'seasonal' uses the X-13 binaries", 
-          "\nprovided by the 'x13binary' package, and does not require ",
-          "\n'X13_PATH' to be set anymore. Only set 'X13_PATH' manually ",
-          "\nif you intend to use your own binaries."
+          "\n\nFrom version 1.2 on, 'seasonal' relies on the 'x13binary' ",
+          "\npackage and does not require 'X13_PATH' to be set anymore. ",
+          "\nOnly set 'X13_PATH' manually if you intend to use your own",
+          "\nbinaries."
         )
       }
     }
   }
+
   if (Sys.getenv("X13_PATH") == ""){
-    # Solaris users get this message (and return())
-    if ((.Platform$OS.type != "windows") && !(Sys.info()["sysname"] %in% c("Darwin", "Linux"))){
-      return(packageStartupMessage("Unusual platform: ", Sys.info()["sysname"], 
+    if (!x13binary::supportedPlatform()){
+      return(packageStartupMessage("Unsupported platform: ", 
+        Sys.info()["sysname"], " ", Sys.info()["release"], 
         "\nFor this platform, the path to the binary executable of X-13",
         "\nhas to be manually specified. For more information, consider", 
         "\nAppendix A of the package vignette:",
@@ -29,9 +27,7 @@
       )
     }
     x13binary::checkX13binary()
-    Sys.setenv(X13_PATH = system.file("bin", package="x13binary"))
-    # Sys.setenv(X13_PATH = x13binary::x13path())
+    Sys.setenv(X13_PATH = x13binary::x13path())
   }
-  # setX13Path()
   checkX13(fullcheck = FALSE, htmlcheck = TRUE)
 }
