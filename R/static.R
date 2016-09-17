08c6fc1472a81edb8d408821c3e3ecb1531aebfb
Christoph <christoph@Christophs-MacBook-Pro.local>
2016-09-18 01:33:25 +0200

    finalize x11.filter argument

--- R/static.R
+++ R/static.R
@@ -1,25 +1,29 @@
 #' Static Call of a seas Object
 #' 
-#' A static call is a static replication of a call. Automatic procedures are 
-#' substituted by the automatically chosen spec-argument options. The call can
-#' be copy/pasted to a script and used for further manipulations or future 
-#' evaluation of the same model.
+#' In a static call, automatic procedures are substituted by the spec-argument
+#' options that have been selcted by the automatic procedures.
+#' 
+#' The call can be copy/pasted to a script and used for further manipulations or
+#' future evaluation of the same model.
 #' 
 #' By default, the static call is tested. It is executed and compared to the 
 #' input call. If the final series is not identical, a message is returned.
 #' 
-#' If \code{coef = TRUE}, the coefficients are fixed as well.
+#' If \code{coef = TRUE}, the coefficients are fixed as well. If 
+#' \code{x11.filter = TRUE}, the X-11 moving averages are fixed as well.
 #' 
 #' @param x an object of class \code{seas}.
-#' @param coef  logical, if \code{TRUE}, the coefficients are treated as fixed, 
+#' @param coef  logical. If \code{TRUE}, the coefficients are treated as fixed, 
 #'   instead of beeing estimated.
+#' @param x11.filter logical. X-11 only. if \code{TRUE}, the X-11 moving 
+#'   averages will be fixed as well. This leads to different filters at
+#'   different stages, and the resulting series can be are slightly different.
+#'   If \code{test = TRUE}, this may cause a warning  message.
 #' @param test logical. By default the static call is executed and compared to 
-#'   the input call. If the final series is not identical, a warning is 
-#'   returned. If \code{FALSE}, the option is disabled.
-#' @param verbose logical, if \code{TRUE}, droped and kept series are listed.
+#'   the input call. If the final series is not identical, a message is 
+#'   returned. If \code{FALSE}, no test is performed (faster).
 #' @param fail logical, if \code{TRUE}, differences will cause an error. Ignored 
 #'   if \code{test = FALSE}.
-#' 
 #' @return Object of class \code{"call"}. Static call of an object of class
 #'   \code{seas}. Can be copy/pasted into an R script.
 #'   
@@ -41,9 +45,12 @@
 #' m <- seas(AirPassengers)
 #' static(m)
 #' static(m, test = FALSE)
+#' 
+#' m <- seas(AirPassengers, x11 = "")
+#' static(m, x11.filter = TRUE)
 #' }
-static <- function(x, coef = FALSE, test = TRUE, verbose = FALSE, fail = FALSE){
-  
+static <- function(x, coef = FALSE, x11.filter = FALSE, test = TRUE, fail = FALSE){
+
   if (!inherits(x, "seas")){
     stop("first argument must be of class 'seas'")
   }
@@ -73,20 +80,20 @@ static <- function(x, coef = FALSE, test = TRUE, verbose = FALSE, fail = FALSE){
 
   lc$transform.function = transformfunction(x)
 
-  # subsitute X-11 filters
-  if (!is.null(x$spc$x11)) {
-    if (is.null(lc$x11.trendma) || lc$x11.trendma == ""){
-      tma <- udg(x, "finaltrendma", fail = FALSE)
-      lc$x11.trendma <- as.numeric(unname(tma))
-    }
-    if (is.null(lc$x11.seasonalma) || lc$x11.seasonalma == ""){
-      sma <- udg(x, "sfmsr", fail = FALSE)
-      if (!is.null(sma)) sma <- paste0("s", sma)
-      lc$x11.seasonalma <- unname(sma)
+  # substitute X-11 filters
+  if (x11.filter){
+    if (!is.null(x$spc$x11)) {
+      if (is.null(lc$x11.trendma) || lc$x11.trendma == ""){
+        tma <- udg(x, "finaltrendma", fail = FALSE)
+        lc$x11.trendma <- as.numeric(unname(tma))
+      }
+      if (is.null(lc$x11.seasonalma) || lc$x11.seasonalma == ""){
+        sma <- udg(x, "sfmsr", fail = FALSE)
+        if (!is.null(sma)) sma <- paste0("s", sma)
+        lc$x11.seasonalma <- unname(sma)
+      }
     }
   }
-
-
 
   if (coef){
     if (!is.null(x$model$regression$b)) {
@@ -98,11 +105,6 @@ static <- function(x, coef = FALSE, test = TRUE, verbose = FALSE, fail = FALSE){
     if (!is.null(x$model$arima$ar)) {
       lc$arima.ar = add_f(x$model$arima$ar)
     }
-  }
-  
-  if (verbose){
-    cat("Droped:", paste(names(as.list(x$call)[!(names(as.list(x$call)) %in% names(lc))]), collapse=", "), "\n")
-    cat("Kept:", paste(names(as.list(x$call)[(names(as.list(x$call)) %in% names(lc))]), collapse=", "), "\n")
   }
 
   z <- as.call(lc)
