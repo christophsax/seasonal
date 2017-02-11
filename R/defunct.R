5d38e7a002cb67a82d8c26d73e0f99347ab69449
Christoph Sax <christoph.sax@gmail.com>
2017-02-11 14:40:49 +0100

    rm inspect stuff

--- R/defunct.R
+++ R/defunct.R
@@ -17,14 +17,34 @@ arimamodel <- function(x){
   msg <- 'udg(x, "x13mdl")'
   .Defunct(msg)
 
-  str <- x$model$arima$model
-  str <- gsub("[ \\(\\)]", "", str)
-  z <- c(substr(str, 1, 1),
-         substr(str, 2, 2),
-         substr(str, 3, 3),
-         substr(str, 4, 4),
-         substr(str, 5, 5),
-         substr(str, 6, 6)
-  )
-  as.numeric(z)
+}
+
+
+#' Defunct Functions
+#' 
+#' The \code{inspect} functions is defunct now. Use 
+#' the extended \code{\link{view}} function instead.
+#' @param x an object of class \code{"seas"}. 
+#' @param fun a function or a list of functions (see details)
+#' @param check.version logical, should the version of shiny be checked
+#' @param quiet logical, if \code{TRUE} (default), error messages from calls in 
+#'   inspect are not shown in the console
+#' @param ... further arguments, passed on to 
+#'   \code{\link[shiny]{runApp}}. (The \code{launch.browser} argument of 
+#'   version 0.8 can be still used that way)
+#' @rdname seasonal-defunct
+#' @seealso \code{\link{view}}, for an extended graphical user interface.
+#' @examples
+#' \dontrun{
+#' m <- seas(AirPassengers)
+#' view(m)
+#' }
+#' @export
+inspect <- function(x, fun = NULL, check.version = TRUE, quiet = TRUE, ...){ 
+  msg <- 
+"This function is defunct. The 'seasonal' packages now uses the 
+graphical user interface from the 'seasonalview' package. 
+Please use instead: \n\n    view(x)"
+  .Defunct(new = "", msg = msg)
+
 }
