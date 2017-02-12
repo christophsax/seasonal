0b886c68feeeb81aeb03b3da04abcd4fc6d4144d
Christoph Sax <christoph.sax@gmail.com>
2017-02-12 09:54:56 +0100

    rd adj

--- R/as.data.frame.R
+++ R/as.data.frame.R
@@ -1,16 +1,18 @@
 #' Coerce Output to \code{data.frame}
 #' 
 #' These methods coerce the output to a \code{data.frame}. This is useful for
-#' further processing. (This is a second attempt of doing that; the first
+#' further processing. (This is a second attempt to do that; the first
 #' experimenal attempt in version 1.4 used an argument \code{data.frame} in the
 #' call to the functions, and is now obsolete. The present approach seems
-#' cleaner, but still consider it as \strong{experimental}.)
+#' cleaner and is likely to stay, but still consider it as 
+#' \strong{experimental}.)
 #' 
 #' The data.frames produced by these functions follow the naming conventions
 #' from the 'broom' package, but do not depend on it otherwise.
 #' 
-#' @param x      an object of class \code{"seas"}, \code{"udg"} or \code{"summary.seas"}, usually, a result of a 
-#'                    call to the functions with the same name.
+#' @param x  an object of class \code{"seas"}, \code{"udg"} or
+#'   \code{"summary.seas"}, usually, the result of a call to the functions
+#'   with the same name.
 #' @param ...    unused.
 #' 
 #' @return a \code{data.frame} without row names. 
