0e165aa98190db75c7174d04cfcf7bdf698d8a4a
Christoph Sax <christoph.sax@gmail.com>
2021-03-03 21:54:25 +0100

    test out = TRUE

--- tests/testthat/test-issues.R
+++ tests/testthat/test-issues.R
@@ -10,3 +10,9 @@ test_that("'fts' import via series works (#240)", {
   m <- seas(AirPassengers, arima.model = '(0 1 1)(0 1 1)', outlier.types = "all", outlier.save = "fts")
   expect_false(any(is.na(m$series$fts[,'t.TC.'])))
 })
+
+test_that("update works with out = TRUE", {
+  m <- seas(mdeaths)
+  expect_s3_class(update(m, out = TRUE), "seas")
+})
+
