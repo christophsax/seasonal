683733a4bd1308dff9db4051df3d3b9e655224b8
Christoph Sax <christoph.sax@gmail.com>
2022-03-22 20:42:07 +0100

    keep SPECTS as CSV, too

---
+++ noinst/specs/update_specs.R
@@ -0,0 +1,11 @@
+
+# Corrections
+# SPECS[1,1]  <- "automdl.autochoice"
+# SPECS$spec <- as.character(SPECS$spec)
+# SPECS$requires <- as.character(SPECS$requires)
+# readr::write_csv(SPECS, "noinst/specs/SPECS.csv")
+
+# load SPECS data from .csv and store as data/SPECS.RData
+SPECS <- read.csv("noinst/specs/SPECS.csv", stringsAsFactors = FALSE)
+
+save(SPECS, "data/SPECS.RData")
