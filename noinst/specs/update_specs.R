8488c7e1c2b1a871a24a0248887c8d69fffd0200
Christoph Sax <christoph.sax@gmail.com>
2022-04-11 12:12:49 +0800

    series: update roxygen header from data

--- noinst/specs/update_specs.R
+++ noinst/specs/update_specs.R
@@ -1,11 +1,56 @@
 
-# Corrections
-# SPECS[1,1]  <- "automdl.autochoice"
-# SPECS$spec <- as.character(SPECS$spec)
-# SPECS$requires <- as.character(SPECS$requires)
-# readr::write_csv(SPECS, "noinst/specs/SPECS.csv")
+# update internal data file ----------------------------------------------------
+
 
 # load SPECS data from .csv and store as data/SPECS.RData
 SPECS <- read.csv("noinst/specs/SPECS.csv", stringsAsFactors = FALSE)
 
 save(SPECS, file = "data/specs.RData")
+
+
+
+
+
+# update roxygen header (carfully review!) -------------------------------------
+
+# Would be nice if we could link or include the description from X-13 manual
+
+library(tidyverse)
+
+txt <- read_lines("R/series.R")
+line0 <- which(txt == "#' **spec** \\tab **long name** \\tab **short name** \\cr")
+
+linen <- which(txt == "#' }")[1]
+txt[line0:linen]
+
+
+#' **spec** \tab **long name** \tab **short name** \cr
+SPECS <- as_tibble(SPECS)
+
+# why this? series, but not save
+# # A tibble: 5 × 6
+#   long                       short spec      is.save is.series requires
+#   <chr>                      <chr> <chr>     <lgl>   <lgl>     <chr>
+# 1 composite.indmovseasrat    ims   composite FALSE   TRUE      ""
+# 2 composite.indreplacsi      id9   composite FALSE   TRUE      ""
+# 3 composite.indresidualseasf irf   composite FALSE   TRUE      ""
+# 4 composite.ratioplotindsa   ir2   composite FALSE   TRUE      ""
+# 5 composite.ratioplotorig    ir1   composite FALSE   TRUE      ""
+
+tbl_txt <-
+  SPECS |>
+  filter(is.series == TRUE) |>
+  filter(is.save == TRUE) |>
+  select(spec, long, short) |>
+  transmute(new = paste("#'", spec, "\\tab", long, "\\tab", short, "\\cr")) |>
+  pull(new)
+
+txt_new <-
+  c(
+    txt[1:line0],
+    tbl_txt,
+    txt[linen:length(txt)]
+  )
+
+
+write_lines(txt_new, "R/series.R")
