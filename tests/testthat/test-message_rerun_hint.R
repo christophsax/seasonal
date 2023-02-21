04ee7cca2d7ddad3f0cd6524c4ac5900e5f023f6
Severin Thöni <severin@cynkra.com>
2023-02-21 13:39:08 +0100

    fix message_rerun_hint with existing arguments

--- tests/testthat/test-message_rerun_hint.R
+++ tests/testthat/test-message_rerun_hint.R
@@ -7,6 +7,15 @@ test_that("message_rerun_hint works", {
   )
 })
 
+test_that("message_rerun_hint with multiple dots", {
+  call <- quote(seas(x))
+
+  expect_message(
+    message_rerun_hint(call, list(foo = "bar", baz = 3)),
+    "seas\\(x, baz = 3, foo = \"bar\"\\)"
+  )
+})
+
 test_that("message_rerun_hint consolidates dots with same name", {
   call <- quote(seas(x))
 
@@ -16,5 +25,17 @@ test_that("message_rerun_hint consolidates dots with same name", {
       list(foo = "bar", foo = "baz")
     ),
     "seas\\(x, foo = c\\(\"bar\", \"baz\"\\)\\)"
+  )
+})
+
+test_that("message_rerun_hint preserves existing args", {
+  call <- quote(seas(x, foo = "yes"))
+
+  expect_message(
+    message_rerun_hint(
+      call,
+      list(foo = "no")
+    ),
+    "seas\\(x, foo = c\\(\"no\", \"yes\"\\)\\)"
   )
 })
