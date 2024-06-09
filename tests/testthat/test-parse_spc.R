db231d7889757d953cb5fdfebada3c9e69ef6da1
Andrea Ranzato <andrea@cynkra.com>
2024-06-09 18:31:50 +0200

    add parse_spc test

---
+++ tests/testthat/test-parse_spc.R
@@ -0,0 +1,16 @@
+test_that("parse_spc accounts for uppercase arguments and spec", {
+  
+  text <- '
+    SERIES{
+    TITLE = "xyz"
+    START = 2014.01
+    PERIOD = 12
+    SAVE = (A1 B1)
+    PRINT = BRIEF
+    NAME = "abc"
+    FILE = "path/to/file.dat"
+  }'
+  
+  expect_snapshot(parse_spc(text))
+  
+})
